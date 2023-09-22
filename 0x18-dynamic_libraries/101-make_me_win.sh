d0e50d40decafcbc45f41e6d633c63587ae8fd9c
Mohamad <108556178+MMKIS007@users.noreply.github.com>
2023-09-22 16:17:29 +0300

    Upd

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P ../ https://github.com/MMKIS007/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=../libgiga.so
+wget -P  /tmp https://github.com/MMKIS007/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
